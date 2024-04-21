be49865d1aaa9d9e752bf7090acecf63ff75f754
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-21 11:18:19 -0700

    Update server.py

--- server.py
+++ server.py
@@ -2,5 +2,10 @@ import socket
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Indicator that message has been received
+print("Message received!")
+
+
+
 # Close socket
 .close()
