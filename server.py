5eda2231df3c9d2c4608a4db481e350c843f51bd
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-23 12:04:51 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import socket
 
+# Create socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Listens for incoming connections, 1 can be changed as the value, keep it that for now
+serverSocket.listen(1)
 
 # Indicator that message has been received
 print("Message received!")
