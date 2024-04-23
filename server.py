59a095ba70ac762087e527cf7f29edbf700f412c
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-23 12:26:56 -0700

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import os
 # Create socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Implement limit of bytes that can be received
 max_bytes = 1024
 
 # Listens for incoming connections, 1 can be changed as the value, keep it that for now
