f0e56010936a1dd4cf473a628b6ed5e32d471756
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-23 12:16:36 -0700

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Specify max amount of bytes to be sent
 max = 1024 
 
-str =
+str = open("untitled.txt", "r")
 
 # Close socket
 clientSocket.close()
