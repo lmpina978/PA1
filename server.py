5d81a4a86385cb7c166f08c6422a1d18986ffa64
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-23 12:08:49 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import socket
 # Create socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+max_bytes = 1024
+
 # Listens for incoming connections, 1 can be changed as the value, keep it that for now
 serverSocket.listen(1)
 
