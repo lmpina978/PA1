ac70e985f52342b45cfcf7ea3ef7dcf979eb9384
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-23 12:15:28 -0700

    Update client.py

--- client.py
+++ client.py
@@ -2,5 +2,10 @@ import socket
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Specify max amount of bytes to be sent
+max = 1024 
+
+str =
+
 # Close socket
 clientSocket.close()
