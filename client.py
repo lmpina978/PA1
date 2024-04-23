4f5cfaf9cae448959b23fa389013c98044198adc
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-23 12:23:23 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import socket 
+import os
 
+# Create client socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Specify max amount of bytes to be sent
 max = 1024 
 
+# Open file for string
 str = open("untitled.txt", "r")
 
+# Get byte amount of file and see if it exceeds max
+
+  
 # Close socket
 clientSocket.close()
