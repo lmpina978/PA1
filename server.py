9afa0b45b47a61d3a7c91f90b19743a554d8772c
bchiucpsc131 <46943746+bchiucpsc131@users.noreply.github.com>
2024-04-24 23:36:32 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,7 @@
 import socket
-import sys
-import os
 
-# Create client socket
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Specify max amount of bytes to be sent in each packet
-max = 1024
-
-# Will count the amount of packets that were sent from client to server
-packetCount = 0
-
-# Open file for string
-txtfile = open("untitled.txt", "r")
-filename = sys.argv[1]
-
-print("Opening " + filename)
-
-txtdata = txtfile.read()
-
-# Check if data is string and get size in bytes
-stringcheck = isinstance(txtdata, str)
-stringsize = os.path.getsize("untitled.txt")
+# Find port to listen to 
 
 
-# Close socket
-clientSocket.close()
+# Create socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
