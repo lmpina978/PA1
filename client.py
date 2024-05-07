01396e22e9389fd7f31ec56ee0dc5fd938499220
DarkLightMatter <jagaytan@csu.fullerton.edu>
2024-05-07 01:40:43 -0700

    Got the two connections for control and data to work

--- client.py
+++ client.py
@@ -6,66 +6,112 @@ BUFFER_SIZE = 1024
 
 def connect_to_server(server_address, server_port):
     # Create a TCP socket for the control channel
-    control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as error:
+        print('Socket creation failed with error %s' %(error))
+    
+    try:
+        server_address = socket.gethostbyname(server_address) 
+    except socket.gaierror: 
+        print ('There was an error resolving the host.')
+        sys.exit(1) 
+
     control_socket.connect((server_address, server_port))
     return control_socket
 
-def send_message(control_socket, message):
-    control_socket.send(message.encode())
+def send_message(any_socket, message):
+    any_socket.send(message.encode())
 
-def receive_message(control_socket):
-    return control_socket.recv(BUFFER_SIZE).decode()
+def receive_message(any_socket):
+    return any_socket.recv(BUFFER_SIZE).decode()
 
-def get_file(control_socket, file_name):
+def get_file(control_socket, file_name, server_address):
     send_message(control_socket, f'GET {file_name}')
     response = receive_message(control_socket)
 
-    if response.startswith('FILE'):
-        file_info = response.split()[1:]
-        file_size = int(file_info[1])
-        print(f'Receiving {file_name} ({file_size} bytes)...')
-        send_message(control_socket, 'START')
-
-        with open(file_name, 'wb') as file:
-            bytes_received = 0
-
-            while bytes_received < file_size:
-                data = control_socket.recv(BUFFER_SIZE)
-                file.write(data)
-                bytes_received += len(data)
+    if response.startswith('PORT'):
+        port_number = response.split()[1]
+        print(port_number)
+        ephPort = int(port_number)
+        print(f'Attempt to connect to port {ephPort}')
+        ephSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ephSocket.connect((server_address, ephPort))
+        response = receive_message(ephSocket)
         
-        print(f'{file_name} received successfully.')
+        if response.startswith('FILE'):
+            file_info = response.split()[1:]
+            file_size = int(file_info[1])
+            print(f'Connected, downloading {file_name} ({file_size} bytes)...')
+            send_message(ephSocket, 'START')
+            
+            with open(file_name, 'wb') as file:
+                bytes_received = 0
+                
+                while bytes_received < file_size:
+                    data = ephSocket.recv(BUFFER_SIZE)
+                    file.write(data)
+                    bytes_received += len(data)
+            
+            print(f'{file_name} downloaded successfully.')
+
+        ephSocket.close()
     else:
         print(response)
 
-def put_file(control_socket, file_name):
+def put_file(control_socket, file_name, server_address):
     if os.path.isfile(file_name):
         file_size = os.path.getsize(file_name)
         send_message(control_socket, f'PUT {file_name}')
         response = receive_message(control_socket)
 
-        if response.startswith('READY'):
-            send_message(control_socket, str(file_size))
-
-            with open(file_name, 'rb') as file:
-                while True:
-                    data = file.read(BUFFER_SIZE)
-                    
-                    if not data:
-                        break
-                    
-                    control_socket.send(data)
+        if response.startswith('PORT'):
+            port_number = response.split()[1]
+            print(port_number)
+            ephPort = int(port_number)
+            print(f'Attempt to connect to port {ephPort}')
+            ephSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ephSocket.connect((server_address, ephPort))
+            print(f'Connected, uploading {file_name} ({int(file_size)} bytes)...')
+            response = receive_message(ephSocket)
             
-            print(f'{file_name} uploaded successfully.')
-        else:
-            print(response)
+            if response.startswith('READY'):
+                send_message(ephSocket, str(file_size))
+
+                with open(file_name, 'rb') as file:
+                    while True:
+                        data = file.read(BUFFER_SIZE)
+                    
+                        if not data:
+                            break
+                    
+                        ephSocket.send(data)
+            
+                print(f'{file_name} uploaded successfully.')
+            else:
+                print(response)
+        
+            ephSocket.close()
     else:
         print(f'Error: {file_name} not found.')
 
-def list_files(control_socket):
+def list_files(control_socket, server_address):
     send_message(control_socket, 'LS')
     response = receive_message(control_socket)
-    print(response)
+
+    if response.startswith('PORT'):
+        port_number = response.split()[1]
+        print(f'Port {port_number}')
+        ephPort = int(port_number)
+        print(f'Attempt to connect to port {ephPort}')
+        ephSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ephSocket.connect((server_address, ephPort))
+        print(f'Connected, listing out the files in the server...')
+        
+        response = receive_message(ephSocket)
+        print(response)
+
+        ephSocket.close()
 
 def quit(control_socket):
     send_message(control_socket, 'QUIT')
@@ -75,83 +121,58 @@ def quit(control_socket):
 
 def main():
     if len(sys.argv) != 3:
-        print('Usage: python3 client.py <host_address> <host_port>')
+        print('Usage: python3 client.py <server_address> <server_port>')
         sys.exit(1)
-
-    host_address = sys.argv[1]
     
+    server_address = sys.argv[1]
+
     try:
-        host_port = int(sys.argv[2])
+        server_port = int(sys.argv[2])
     except ValueError:
         print('Error: Server port must be an integer.')
         sys.exit(1)
    
-    control_socket = connect_to_server(host_address, host_port)
-    
-    while True:
-        command = input('ftp> ').strip().split()
-        valid_command = len(command) == 2
-        print(len(command))
+    print(f'Connecting with server on port {server_port}...')
+    control_socket = connect_to_server(server_address, server_port)
+    send_message(control_socket, 'CONNECT')
 
-        if not command:
-            continue
-        elif command[0].lower() == 'put':
-            if(valid_command):
-                file_name = command[1]
-                put_file(control_socket, file_name)
+    if(receive_message(control_socket) == 'READY'):
+        print('Server is ready')
+
+        while True:
+            command = input('ftp> ').strip().split()
+            print(len(command))
+            
+            if not command:
+                continue
+            elif command[0].lower() == 'put':
+                if(len(command) == 2):
+                    file_name = command[1]
+                    put_file(control_socket, file_name, server_address)
+                else:
+                    print('ERROR: Please write the command in the format of "put <fileName>".')
+            elif command[0].lower() == 'get':
+                if(len(command) == 2):
+                    file_name = command[1]
+                    get_file(control_socket, file_name, server_address)
+                else:
+                    print('ERROR: Please write the command in the format of "get <fileName>".')
+            elif command[0].lower() == 'ls':
+                if(len(command) == 1):
+                    list_files(control_socket, server_address)
+                else:
+                    print("Error")
+            elif command[0].lower() == 'quit':
+                if(len(command) == 1):
+                    quit(control_socket)
+                    break
+                else:
+                    print("error")
             else:
-                print('ERROR: Please write the command in the format of "put <fileName>".')
-        elif command[0].lower() == 'get':
-            if(valid_command):
-                file_name = command[1]
-                get_file(control_socket, file_name)
-            else:
-                print('ERROR: Please write the command in the format of "get <fileName>".')
-        elif command[0].lower() == 'ls':
-            if(valid_command):
-                list_files(control_socket)
-            else:
-                print("Error")
-        elif command[0].lower() == 'quit':
-            if(valid_command):
-                quit(control_socket)
-                break
-            else:
-                print("error")
-        else:
-            print('ERROR: Invalid command. Valid commands are "ls, get, put, quit".')
+                print('ERROR: Invalid command. Valid commands are "ls, get, put, quit".')
+    else:
+        print(f'Failed to connect with server on port {server_port}')
+    
 
 if __name__ == '__main__':
     main()
-
-# Old Code:
-# import socket
-# import sys
-# import os
-
-# # Create client socket
-# clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# # Specify max amount of bytes to be sent in each packet
-# max = 1024
-
-# # Will count the amount of packets that were sent from client to server
-# packetCount = 0
-
-# # Open file for string
-# txtfile = open("untitled.txt", "r")
-# filename = sys.argv[1]
-
-# print("Opening " + filename)
-
-# txtdata = txtfile.read()
-
-# # Check if data is string and get size in bytes
-# stringcheck = isinstance(txtdata, str)
-# stringsize = os.path.getsize("untitled.txt")
-
-
-
-# print("A total of " + packetCount + " packets and " + "0 bytes have been sent.")
-# # Close socket
-# clientSocket.close()
--- server.py
+++ server.py
@@ -13,15 +13,21 @@ def start_server(server_port):
 
     while True:
         control_socket, client_address = server_socket.accept()
-        print(f'Connection established with {client_address}')
-        handle_client(control_socket, client_address)
-        control_socket.close()
 
-def send_message(control_socket, message):
-    control_socket.send(message.encode())
+        if(receive_message(control_socket) == 'CONNECT'):
+            print(f'Connection established with {client_address}')
+            send_message(control_socket, 'READY')
+            handle_client(control_socket, client_address)
+            control_socket.close()
+        else:
+            print(f'Failed to connect with {client_address}')
+            send_message(control_socket, 'FAILURE')
 
-def receive_message(control_socket):
-    return control_socket.recv(BUFFER_SIZE).decode()
+def send_message(any_socket, message):
+    any_socket.send(message.encode())
+
+def receive_message(any_socket):
+    return any_socket.recv(BUFFER_SIZE).decode()
 
 def handle_client(control_socket, client_address):
     while True:
@@ -41,13 +47,23 @@ def handle_client(control_socket, client_address):
             list_files(control_socket)
 
 def send_file(control_socket, file_name):
-    print("send file good")
+    print("In send_file function")
 
     if os.path.isfile(os.path.join('files', file_name)):
-        print("beginning verified file name is here in files")
-        send_message(control_socket, f'FILE {file_name} {os.path.getsize(os.path.join("files", file_name))}')
-        response = receive_message(control_socket)
-        print("verified file name is here in files")
+        # Establish a new data channel
+        data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        data_socket.bind(('', 0))  # Bind to an ephemeral port
+        data_socket.listen(1)
+
+        data_port = data_socket.getsockname()[1]
+        send_message(control_socket, f'PORT {data_port}')
+
+        data_connection, data_address = data_socket.accept()
+        print(f'Data connection established with {data_address} for {file_name}')
+
+        send_message(data_connection, f'FILE {file_name} {os.path.getsize(os.path.join("files", file_name))}')
+        response = receive_message(data_connection)
+        print("Data_socket good in send_file")
 
         if response == 'START':
             print("It good")
@@ -59,32 +75,62 @@ def send_file(control_socket, file_name):
                     if not data:
                         break
                     
-                    control_socket.send(data) ##Issue here again
+                    data_connection.send(data)
 
-            #send_message(control_socket, 'SUCCESS File sent.')
+            #send_message(data_connection, 'SUCCESS File sent.')
         else:
-            send_message(control_socket, 'FAILURE Failed to start file transfer.')
+            send_message(data_connection, 'FAILURE Failed to start file transfer.')
+        
+        data_connection.close()
+        data_socket.close()
     else:
         send_message(control_socket, 'FAILURE File not found.')
 
 def receive_file(control_socket, file_name):
-    send_message(control_socket, 'READY')
-    file_size = int(receive_message(control_socket))
+    # Establish a new data channel
+    data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    data_socket.bind(('', 0))  # Bind to an ephemeral port
+    data_socket.listen(1)
+
+    data_port = data_socket.getsockname()[1]
+    send_message(control_socket, f'PORT {data_port}')
+
+    data_connection, data_address = data_socket.accept()
+    print(f'Data connection established with {data_address} for {file_name}')
+
+    send_message(data_connection, 'READY')
+    file_size = int(receive_message(data_connection))
     bytes_received = 0
     #send_message(control_socket, 'START ')
 
     with open(os.path.join('files', file_name), 'wb') as file:
         while bytes_received < file_size:
-            data = control_socket.recv(BUFFER_SIZE)
+            data = data_connection.recv(BUFFER_SIZE)
             file.write(data)
             bytes_received += len(data)
-
+    
+    data_connection.close()
+    data_socket.close()
     #send_message(control_socket, 'SUCCESS File received.')
 
 def list_files(control_socket):
+    # Establish a new data channel
+    data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    data_socket.bind(('', 0))  # Bind to an ephemeral port
+    data_socket.listen(1)
+
+    data_port = data_socket.getsockname()[1]
+    send_message(control_socket, f'PORT {data_port}')
+
+    data_connection, data_address = data_socket.accept()
+    print(f'Data connection established with {data_address}')
+
     file_list = ' '.join(os.listdir(os.path.join(os.getcwd(), 'files')))
     print(file_list)
-    send_message(control_socket, file_list)
+    send_message(data_connection, file_list)
+
+    data_connection.close()
+    data_socket.close()
 
 def main():
     if len(sys.argv) != 2:
@@ -101,12 +147,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# Old code:
-# import socket
-
-# # Find port to listen to 
-
-
-# # Create socket
-# serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
